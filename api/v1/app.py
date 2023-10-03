d4ccf41afb506a9575fdb096cc64fbf4c475e4fe
shield <myshield02@gmail.com>
2023-10-03 03:03:52 +0100

    handling 404 not found error

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,10 +14,19 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error Response
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     """ Application """
@@ -28,9 +37,3 @@ if __name__ == "__main__":
     if not port:
         port = '5000'
     app.run(host=host, port=port, threaded=True)
-
-@app.errorhandler(404)
-def not_found(error):
-    """ 404 Error Response
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
